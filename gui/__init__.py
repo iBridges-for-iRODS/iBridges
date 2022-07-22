8bebeab75ae2c5b278420746ad593909014a1961
jpmcfarland <j.p.mc.farland@rug.nl>
2022-07-22 15:05:07 +0200

    [#2] Add basic imports to package support files

---
+++ gui/__init__.py
@@ -0,0 +1,17 @@
+"""iBridges GUI modules
+"""
+from . import checkableFsTree
+from . import continousUpload
+from . import dataTransfer
+from . import elabUpload
+from . import irodsBrowser
+from . import irodsCreateTicket
+from . import irodsDataCompression
+from . import irodsInfo
+from . import irodsSearch
+from . import irodsTicketLogin
+from . import irodsTreeView
+from . import irodsUpDownload
+# from . import mainmenu
+from .mainmenu import mainmenu
+from . import popupWidgets
---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+"""iBridges utility modules
+"""
+
+from . import elabConnector
+from . import irodsConnectorAnonymous
+from . import irodsConnectorIcommands
+from . import irodsConnector
+from . import utils
