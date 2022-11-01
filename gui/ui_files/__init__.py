fd1f615a5081bcabbfceda64d77079e7c06a9d0b
J.P. Mc Farland <j.p.mc.farland@rug.nl>
2022-11-01 15:21:34 +0100

    [chstaiger#26] Add files created upstream

---
+++ gui/ui_files/__init__.py
@@ -0,0 +1,18 @@
+"""
+
+"""
+from . import createCollection
+from . import dataTransferState
+from . import irodsIndexPopup
+from . import irodsLogin
+from . import MainMenu
+from . import searchDialog
+from . import tabBrowser
+from . import tabDataCompression
+from . import tabELNData
+from . import tabFederations
+from . import tabInfo
+from . import tabPage
+from . import tabTicketAccess
+from . import tabTicketCreate
+from . import tabUpDownload
