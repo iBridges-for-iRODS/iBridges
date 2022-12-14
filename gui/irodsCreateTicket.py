3f063845331f38661e72f88ec2bce2e1dfc7dd09
J.P. Mc Farland <j.p.mc.farland@rug.nl>
2022-12-14 15:47:49 +0100

    [chstaiger#26] Rename some identifiers

--- gui/irodsCreateTicket.py
+++ gui/irodsCreateTicket.py
@@ -34,39 +34,36 @@ class irodsCreateTicket(QWidget, Ui_tabticketCreate):
         self.irodsFsTreeView.setColumnHidden(3, True)
         self.irodsFsTreeView.setColumnHidden(4, True)
 
-        self.createTicketButton.clicked.connect(self.createTicket)
+        self.createTicketButton.clicked.connect(self.create_ticket)
 
-    def createTicket(self):
+    def create_ticket(self):
         self.infoLabel.clear()
         self.ticketInfoBrowser.clear()
         self.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.WaitCursor))
         self.createTicketButton.setEnabled(False)
-
-        # gather info
+        # Gather info.
         indexes = self.irodsFsTreeView.selectedIndexes()
-        path = ''
+        obj_path = ''
         if len(indexes):
-            path = self.irodsmodel.irods_path_from_tree_index(indexes[0])
-        if not self.ic.session.collections.exists(path):
-            self.infoLabel.setText("ERROR: Please select a collection.")
+            obj_path = self.irodsmodel.irods_path_from_tree_index(indexes[0])
+        if not self.ic.session.collections.exists(obj_path):
+            self.infoLabel.setText('ERROR: Please select a collection.')
             self.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.ArrowCursor))
             self.createTicketButton.setEnabled(True)
             return
-        acls = [(acl.user_name, acl.access_name) for acl in self.ic.get_permissions(path)]
-        if (self.ic.session.username, "own") in acls:
+        acls = [(acl.user_name, acl.access_name) for acl in self.ic.get_permissions(obj_path)]
+        if (self.ic.session.username, 'own') in acls:
             date = self.calendar.selectedDate()
             # format of time string for irods: 2012-05-07.23:00:00
-            expiryString = str(date.toPyDate())+'.23:59:59'
-            ticket, expiryDate = self.ic.createTicket(path, expiryString)
-            self.ticketInfoBrowser.append("iRODS server: \t"+self.ic.session.host)
-            self.ticketInfoBrowser.append("iRODS path:\t"+path)
-            self.ticketInfoBrowser.append("iRODS Ticket:\t"+ticket)
-            if self.ic.__name__ == "IrodsConnector":
-                self.ticketInfoBrowser.append("Expiry date:\tNot set (linux only)")
-            else:
-                self.ticketInfoBrowser.append("Expiry date:\t"+expiryDate)
+            expiry_string = f'{date.toPyDate()}.23:59:59'
+            ticket_name, expiration_set = self.ic.create_ticket(obj_path, expiry_string)
+            self.ticketInfoBrowser.append(f'iRODS server:\t{self.ic.session.host}')
+            self.ticketInfoBrowser.append(f'iRODS path:\t{obj_path}')
+            self.ticketInfoBrowser.append(f'iRODS Ticket:\t{ticket_name}')
+            if expiration_set:
+                self.ticketInfoBrowser.append(f'Expiry date:\t{expiry_string}')
         else:
-            self.infoLabel.setText("ERROR: Insufficient rights, you need to be owner.")
+            self.infoLabel.setText('ERROR: Insufficient rights, you need to be owner.')
             self.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.ArrowCursor))
             self.createTicketButton.setEnabled(True)
             return
