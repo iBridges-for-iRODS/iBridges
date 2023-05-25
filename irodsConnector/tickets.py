5282dac0725fd12e5be1037ada46982e6cb815e9
J.P. Mc Farland <j.p.mc.farland@rug.nl>
2023-05-25 17:54:50 +0200

    [chStaiger#103] Remove Ticket iCommand functionality

--- irodsConnector/tickets.py
+++ irodsConnector/tickets.py
@@ -43,41 +43,13 @@ class Tickets(object):
             (str, bool)
 
         """
-        ticket_id = ''.join(choice(ascii_letters) for _ in range(20))
-        ticket = irods.ticket.Ticket(self.sess_man.irods_session, ticket_id)
+        ticket = irods.ticket.Ticket(self.sess_man.irods_session)
         ticket.issue('read', obj_path)
         logging.info('CREATE TICKET: %s: %s', ticket.ticket, obj_path)
         expiration_set = False
         if expiry_string != '':
             try:
-                expiration_set = self._modify_ticket(ticket, expiry_string)
+                expiration_set = ticket.modify('expire', expiry_string) == ticket
             except Exception as error:
                 logging.info('Could not set expiration date: %s', str(error))
         return ticket.ticket, expiration_set
-
-    def _modify_ticket(self, ticket: irods.ticket.Ticket, expiry_string: str) -> bool:
-        """Modify iRODS `ticket` updating the expriration date.
-
-        Parameters
-        ----------
-        ticket : irods.ticket.Ticket
-            iRODS ticket to be modified.
-        expiry_string : str
-           Expiration date in the form: CCYY-MM-DD.hh:mm:ss.
-
-        Returns
-        -------
-        bool
-            If `ticket` successfully modified.
-
-        """
-        # TODO improve error handling, if necessary
-        if not Icommands.IrodsConnectorIcommands.icommands():
-            return ticket.modify('expire', expiry_string) == ticket
-        else:
-            command = f'iticket mod {ticket.ticket} expire {expiry_string}'
-            p = Popen(
-                command, stdout=PIPE, stderr=PIPE,
-                shell=True)
-            _, err = p.communicate()
-            return err == b''
