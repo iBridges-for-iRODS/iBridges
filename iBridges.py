4ff6472f2e219b8d41170e508049cb44f3220c85
chstaiger <staigerchristine@gmail.com>
2023-06-01 09:52:05 +0200

    Init of irodsConnector as part of the context in main

--- iBridges.py
+++ iBridges.py
@@ -128,7 +128,6 @@ class IrodsLoginWindow(PyQt6.QtWidgets.QDialog,
         # Replacement connector (required for new sessions)
         if not self.context.irods_connector:
             logging.debug('Setting new instance of the IrodsConnector')
-            self.context.irods_connector = irodsConnector.manager.IrodsConnector()
         irods_env_file = self.irods_path.joinpath(self.envbox.currentText())
         self.context.irods_env_file = irods_env_file
         logging.debug('IRODS ENVIRONMENT FILE SET: %s', irods_env_file.name)
@@ -226,6 +225,7 @@ def main():
     utils.utils.init_logger(THIS_APPLICATION)
     # Singleton Context
     context = utils.context.Context()
+    context.irods_connector = irodsConnector.manager.IrodsConnector()
     context.application_name = THIS_APPLICATION
     # Context is required to get the log_level from the configuration.
     # Here, it is taken from the configuration if not specified.
