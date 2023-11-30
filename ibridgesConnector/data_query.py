853d3df138ea280aa9ef8c8ed58f9f67fe4604c9
chstaiger <staigerchristine@gmail.com>
2023-11-30 15:41:00 +0100

    ibridgesConnector framework

---
+++ ibridgesConnector/data_query.py
@@ -0,0 +1,106 @@
+""" query operations
+"""
+from . import keywords as kw
+from . import session
+
+
+class Query(object):
+    """Irods Query operations """
+
+    def __init__(self, sess_man: session.Session):
+        """ iRODS data operations initialization
+
+            Parameters
+            ----------
+            sess_man : session.Session
+                instance of the Session class
+
+        """
+        self.sess_man = sess_man
+
+    def search(self, key_vals: dict = None) -> list:
+        """Given a dictionary with metadata attribute names as keys and
+        associated values, query for collections and data objects that
+        fullfill the criteria.  The key 'checksum' will be mapped to
+        DataObject.checksum, the key 'path' will be mapped to
+        Collection.name and the key 'object' will be mapped to
+        DataObject.name:
+
+        {
+            'checksum': '',
+            'key1': 'val1',
+            'key2': 'val2',
+            'path': '',
+            'object': '',
+        }
+
+        By Default, if `key_vals` is empty, all accessible collections
+        and data objects will be returned.
+
+        Parameters
+        ----------
+        key_vals : dict
+            Attribute name mapping to values.
+
+        Returns
+        -------
+        list: [[Collection name, Object name, checksum]]
+
+        """
+        coll_query = None
+        data_query = None
+        # data query
+        if 'checksum' in key_vals or 'object' in key_vals:
+            data_query = self.sess_man.irods_session.query(
+                kw.COLL_NAME, kw.DATA_NAME, kw.DATA_CHECKSUM)
+            if 'object' in key_vals:
+                if key_vals['object']:
+                    data_query = data_query.filter(
+                        kw.LIKE(kw.DATA_NAME, key_vals['object']))
+            if 'checksum' in key_vals:
+                if key_vals['checksum']:
+                    data_query = data_query.filter(kw.LIKE(
+                        kw.DATA_CHECKSUM, key_vals['checksum']))
+        else:
+            coll_query = self.sess_man.irods_session.query(kw.COLL_NAME)
+            data_query = self.sess_man.irods_session.query(
+                kw.COLL_NAME, kw.DATA_NAME, kw.DATA_CHECKSUM)
+
+        if 'path' in key_vals and key_vals['path']:
+            if coll_query:
+                coll_query = coll_query.filter(kw.LIKE(
+                    kw.COLL_NAME, key_vals['path']))
+            data_query = data_query.filter(kw.LIKE(
+                kw.COLL_NAME, key_vals['path']))
+        for key in key_vals:
+            if key not in ['checksum', 'path', 'object']:
+                if data_query:
+                    data_query.filter(kw.META_DATA_ATTR_NAME == key)
+                if coll_query:
+                    coll_query.filter(kw.META_COLL_ATTR_NAME == key)
+                if key_vals[key]:
+                    if data_query:
+                        data_query.filter(
+                            kw.META_DATA_ATTR_VALUE == key_vals[key])
+                    if coll_query:
+                        coll_query.filter(
+                            kw.META_COLL_ATTR_VALUE == key_vals[key])
+        results = [['', '', ''], ['', '', ''], ['', '', '']]
+        coll_batch = [[]]
+        data_batch = [[]]
+        # Return only 100 results.
+        if coll_query:
+            num_colls = len(list(coll_query))
+            results[0] = [f'Collections found: {num_colls}', '', '']
+            coll_batch = list(coll_query.get_batches())
+        if data_query:
+            num_objs = len(list(data_query))
+            results[1] = [f'Objects found: {num_objs}', '', '']
+            data_batch = list(data_query.get_batches())
+        for res in coll_batch[0][:50]:
+            results.append([res[list(res.keys())[0]], '', ''])
+        for res in data_batch[0][:50]:
+            results.append([res[list(res.keys())[0]],
+                            res[list(res.keys())[1]],
+                            res[list(res.keys())[2]]])
+        return results
---
+++ ibridgesConnector/manager.py
@@ -0,0 +1,159 @@
+class ibridges():
+    """
+    Implements further policies and specific behaviour on the irodsConnector.
+    """
+
+    def __init__(self, ibridges_conf: utils.path.LocalPath, 
+                 irods_conf: utils.path.LocalPath):
+        """
+        Read in configuration and make parameters accessible, 
+        Read in irods_conf as dictionary
+        """
+
+    def __del__(self):
+        """
+        Cleanup irods connection and itself
+        """
+    
+    @property
+    def ibridges_config(self):
+        """
+        Return current configuration as dictionary
+        """
+
+    @property
+    def davrods(self):
+        """
+        Return davrods server from configuration file
+        """
+    
+    @davrods.setter
+    def davrods(self, davrods_server: str):
+        """
+        Overwrite current davrods in configuration dictionary
+        """
+
+    @property
+    def check_free_space(self):
+        """
+        Return check_free_space from configration file
+        """
+
+    @check_free_space.setter
+    def check_free_space(self, check: bool):
+        """
+        Overwrite current check_free_space in configuration dictionary
+        """
+
+    @property
+    def force_transfers(self):
+        """
+        Return force_transfers from configration file
+        """
+
+    @force_transfers.setter
+    def force_transfers(self, force: bool):
+        """
+        Overwrite current force_transfers in configuration dictionary
+        """
+
+    @property
+    def irods_conf(self):
+        """
+        Returns irods_conf dictionary 
+        """
+
+    @irods_conf.setter(self, irods_conf_path: utils.path.LocalPath):
+        """
+        Loads a new irods environment, deletes a current existing irods connection.
+        """
+
+    def connect(self):
+        """
+        Creates an irodsConnector from the irods_conf, ensures that the connection is active
+        """
+        # read cached password
+        # try to instantiate irodsConnector from irods_env_file and cached password
+        # if fail on Authentication or no cached passowrd, ask for password and instantiate
+    
+    def disconnect(self):
+        """
+        Diconnects (deletes) current irods session
+        """
+        # remove irodsConnector.session and make invalid
+
+
+    def authenticate(self, password):
+        cached_password = get_cached_password()
+
+        # if password equals cached password --> login with irods env file only
+        if cached_password == password:
+            sess = Session(irods_conf_path)
+            result = sess.connect()
+        # if password differs from cached password --> login with env + password
+        else:
+            sess = Session(irods_conf, password=password)
+            result = sess.connect()
+        return result
+
+
+    def get_cached_password(self) -> str:
+        """Scrape the cached password from the iRODS authentication file,
+        if it exists.
+
+        Returns
+        -------
+        str
+            Cached password or null string.
+
+        """
+        irods_auth_file = os.environ.get('IRODS_AUTHENTICATION_FILE', None)
+        if irods_auth_file is None:
+            irods_auth_file = context.irods_env_file.path.parent.joinpath(".irodsA")
+        if utils.path.LocalPath(irods_auth_file).exists():
+            with open(irods_auth_file, encoding='utf-8') as authfd:
+                return irods.password_obfuscation.decode(authfd.read())
+        return ''
+
+    def write_password(self):
+        """Store the password in the iRODS
+        authentication file in obfuscated form.
+
+        """
+        connection = self._irods_session.pool.get_connection()
+        pam_passwords = self._irods_session.pam_pw_negotiated
+        if len(pam_passwords):
+            irods_auth_file = self._irods_session.get_irods_password_file()
+            with open(irods_auth_file, 'w', encoding='utf-8') as authfd:
+                authfd.write(
+                    irods.password_obfuscation.encode(pam_passwords[0]))
+        else:
+            logging.info('WARNING -- unable to cache obfuscated password locally')
+        connection.release()
+
+    def diff_irods_local(self, local_paths: list, irods_paths: list) -> list:
+        """Given a list of file paths and object paths, return those with a different checksum,
+        or are not present locally
+        """
+
+    def diff_local_irods(self, irods_paths: list, local_paths: list) -> list:
+        """ given a list of objects and files, return those with a different checksum, or are not
+        present on iRODS
+        """
+
+    def writeable_resources(self, size=0: int):
+        """Return only writeable resources.
+        size: Required space in bytes
+        """
+        # Get all root resources
+        # If ibridges config is set to force_transfers = False, only return root resources
+        # which have free_space > size
+
+    def search_data(self, key_vals: dict = None): -> list:
+        """Given a dictionary with metadata attribute names as keys and
+        associated values, query for collections and data objects that
+        fullfill the criteria.
+        """
+
+### link all functions from irodsConnector here
+
