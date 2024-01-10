10affd008a39733749e8f484d8ba9f320c12794e
chstaiger <staigerchristine@gmail.com>
2024-01-10 16:57:56 +0100

    mplement collection up and download.

--- ibridges/irodsconnector/data_operations.py
+++ ibridges/irodsconnector/data_operations.py
@@ -2,10 +2,12 @@
 """
 from typing import Optional, Union
 from pathlib import Path
+from os import walk, makedirs
 
 import irods.collection
 import irods.data_object
 import irods.exception
+from irods.models import DataObject
 
 from ibridges.irodsconnector import keywords as kw
 from ibridges.irodsconnector.session import Session
@@ -146,8 +148,46 @@ class DataOperations():
 
         self.session.irods_session.data_objects.get(str(irods_path), local_path, **options)
 
+    def upload_collection(self, local_path: Path, irods_path: IrodsPath, 
+                          resc_name: str = '', options: Optional[dict] = None):
+        # get all files and their relative path to local_path
+        if not local_path.is_dir():
+            raise ValueError("local_path must be a directory.")
+
+        files = [] # (rel_path, name)
+
+        for w in walk(local_path):
+            files.extend([(w[0].removeprefix(str(local_path)), f) for f in w[2]])
+
+        if not irods_path.is_collection():
+            self.session.irods_session.collections.create(str(irods_path))
+        
+        for f in files:
+            # ensure iRODS collection exists
+            self.session.irods_session.collections.create(str(irods_path) + '/' + f[0])
+            # call irods put for Path(local_path, f[0], f[1]) to destination collection
+            dest = IrodsPath(self.session, str(irods_path), f[0].lstrip('/'))
+            self.irods_put(Path(str(local_path), f[0].lstrip('/'), f[1]), dest, 
+                           resc_name, options)
+
+    def download_collection(self, irods_path: IrodsPath, local_path: Path,
+                            overwrite: bool=False, options: Optional[dict]=None):
+        if not irods_path.is_collection():
+            raise ValueError("irods_path must be a collection.")
+
+        # get all data objects
+        coll = self.get_collection(irods_path)
+        all_objs = self._get_data_objects(coll)
+
+        for o in all_objs:
+            # ensure local folder exists
+            dest = Path(local_path, o[0].removeprefix(str(irods_path)).lstrip('/'))
+            if not dest.is_dir():
+                os.makedirs(dest)
+            self.irods_get(IrodsPath(self.session, o[0], o[1]), local_path, overwrite, options)
+
     def get_size(self, item: Union[irods.data_object.iRODSDataObject,
-                                   irods.collection.iRODSCollection]) -> int:
+                             irods.collection.iRODSCollection]) -> int:
         """Collect the sizes of a data object or a
         collection.
 
@@ -165,13 +205,36 @@ class DataOperations():
         if self.has_type_dataobj(item):
             return item.size
         elif self.has_type_collection(item):
-            irods_size = 0
-            subcollections = [item]
-            while len(subcollections) > 0:
-                coll = subcollections.pop()
-                for obj in coll.data_objects:
-                    irods_size+= obj.size
-                subcollections.extend(coll.subcollections)
-            return irods_size
+            all_objs = self._get_data_objects(item)
+            return sum([size for _, _, size in all_objs])
         else:
             raise ValueError("Item must be an iRODS object or iRODS collection.")
+
+
+    def _get_data_objects(self, coll: irods.collection.iRODSCollection) -> list[str, str, int]:
+        """Retrieve all data objects in a collection and all its subcollections.
+        
+        Parameters
+        ----------
+        coll : irods.collection.iRODSCollection
+            The collection to search for all data pbjects
+
+        Returns
+        -------
+        list of all data objects
+            [(cllection path, name, size)]
+        """
+
+        # all objects in the collection
+        objs = [(obj.collection.path, obj.name, obj.size) for obj in coll.data_objects]
+
+        # all objects in subcollections
+        data_query = self.session.irods_session.query(kw.COLL_NAME, kw.DATA_NAME, 
+                                                      DataObject.size)
+        data_query = data_query.filter(kw.LIKE(kw.COLL_NAME, coll.path+"/%"))
+        for res in data_query.get_results():
+            path, name, size = res.values()
+            objs.append((path, name, size))
+
+        return objs
+        
