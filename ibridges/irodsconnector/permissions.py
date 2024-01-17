3f5cf0aa1ecd9e2b7bc11a6c75746d502cd25d81
chStaiger <staigerchristine@gmail.com>
2024-01-17 11:15:45 +0100

    Permissions (#22) (#24)
    * changes
    
    * repr
    
    * streamlined
    
    * notebook
    
    * notebook
    
    * notebook
    
    * pylint
    
    * repr
    
    * better repr
    
    * Editing the Permissions section.
    
    * Changing the repr of th permissions
    
    * repr -> str
    
    * str reformat
    
    * removed available permissions compat
    
    * Permission --> Permissions
    
    ---------
    
    Co-authored-by: qubixes <44498096+qubixes@users.noreply.github.com>
    Co-authored-by: maarten schermer <m.d.schermer@uu.nl>

---
+++ ibridges/irodsconnector/permissions.py
@@ -0,0 +1,44 @@
+""" permission operations """
+from typing import Iterator
+import irods.access
+import irods.collection
+import irods.exception
+import irods.session
+from collections import defaultdict
+
+class Permissions():
+    """Irods permissions operations"""
+
+    def __init__(self, session, item) -> None:
+        self.session = session
+        self.item = item
+
+    def __iter__(self) -> Iterator:
+        for perm in self.session.irods_session.permissions.get(self.item):
+            yield perm
+
+    def __str__(self) -> str:
+        acl_dict = defaultdict(list)
+        for p in self:
+            acl_dict[f'{p.user_name}#{p.user_zone}'].append(f'{p.access_name}\t{p.user_type}')
+        acl = ''
+        for key, value in sorted(acl_dict.items()):
+            v_str= '\n\t'.join(value)
+            acl += f'{key}\n\t{v_str}\n'
+
+        if isinstance(self.item, irods.collection.iRODSCollection):
+            coll = self.session.irods_session.collections.get(self.item.path)
+            acl += f"inheritance {coll.inheritance}\n"
+
+        return acl
+
+    @property
+    def available_permissions(self) -> dict:
+        """Get available permissions"""
+        return self.session.irods_session.available_permissions
+
+    def set(self, perm: str, user: str = '', zone: str = '',
+            recursive: bool = False, admin: bool = False) -> None:
+        """Set permissions (ACL) for an iRODS collection or data object."""
+        acl = irods.access.iRODSAccess(perm, self.item.path, user, zone)
+        self.session.irods_session.permissions.set(acl, recursive=recursive, admin=admin)
