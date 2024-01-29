5b8615555582072f57fa83e8ed7d832b75f1be34
Raoul Schram <r.d.schram@uu.nl>
2024-01-29 16:30:52 +0100

    Add tests for joinpath

--- tests/test_irodspath.py
+++ tests/test_irodspath.py
@@ -49,26 +49,15 @@ def test_absolute_path(input, abs_path, name, parent):
     assert ipath.name == name
     assert isinstance(ipath.parent, IrodsPath)
     assert str(ipath.parent._path) == parent, str(ipath.parent)
-    # assert irods_path.absolute_path() == session.home()+"/"+dirname+"/"+filename
 
-# def test_name():
-#     assert irods_path.name == filename
-
-# def test_parent():
-#     assert irods_path.parent == session.home()+"/"+dirname
-
-# def test_raw_paths():
-#     assert irods_path._raw_paths == ['', mocksession.zone, "home", 
-#                                      mock_session.user, dirname, filename]
-
-# def test_join_win():
-#     assert irods_path.joinpath(windows_path)._raw_paths == ['', mocksession.zone, "home",
-#                                                             mock_session.user, dirname, filename,
-#                                                             "windows", "path", "directory"]
-
-# def test_join_linux():
-#     assert irods_path.joinpath(linux_path)._raw_paths == ['', mocksession.zone, "home",
-#                                                           mock_session.user, dirname, filename,
-#                                                           "linux", "or", "mac", "path"]
-
-
+@mark.parametrize(
+    "path,to_join,result",
+    [
+        ("/etc", ["test"], "/etc/test"),
+        ("123", ["test", "test2"], "123/test/test2"),
+        ("~", [IrodsPath(123, "test")], "~/test")
+    ]
+)
+def test_join_path(path, to_join, result):
+    irods_path = IrodsPath(123, path)
+    assert str(irods_path.joinpath(*to_join)._path) == result
