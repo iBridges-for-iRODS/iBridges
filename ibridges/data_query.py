e336b2d0800fab8d83afe999732500df770127c3
chstaiger <staigerchristine@gmail.com>
2024-02-06 17:05:53 +0100

    Adding data_query and update tutorial

--- ibridges/data_query.py
+++ ibridges/data_query.py
@@ -5,7 +5,7 @@ from ibridges import Session
 
 
 def data_query(session: Session, path: Optional[str] = None, checksum: Optional[str] = None, 
-               key_vals: Optional[dict] = None) -> list:
+               key_vals: Optional[dict] = {}) -> list[tuple[str, str, str]]:
     """Retrieves all collections and data objects (the absolute collection path, 
     data object or collection name) to the given user-defined and system metadata.
     By Default all accessible collections and data objects will be returned.
@@ -22,38 +22,33 @@ def data_query(session: Session, path: Optional[str] = None, checksum: Optional[
 
     Returns
     -------
-    list: [[Collection name, Object name, checksum]]
+    list: [(Collection name, Object name, checksum)]
 
     """
-    coll_query = None
-    data_query = None
-    
-    if checksum:
-        # data query
-        data_query = self.sess_man.irods_session.query(kw.COLL_NAME, kw.DATA_NAME, 
-                                                       kw.DATA_CHECKSUM)
-        data_query = data_query.filter(kw.LIKE(kw.DATA_CHECKSUM, checksum))
-    else:
-        # data query and coll query
-        coll_query = self.sess_man.irods_session.query(kw.COLL_NAME)
-        data_query = self.sess_man.irods_session.query(
-            kw.COLL_NAME, kw.DATA_NAME, kw.DATA_CHECKSUM)
+    if path is None and checksum is None and key_vals == {}:
+        raise ValueError("QUERY DATA: No query criteria set.")
+
+    coll_query = session.irods_session.query(kw.COLL_NAME)
+    data_query = session.irods_session.query(kw.COLL_NAME, kw.DATA_NAME,
+                                             kw.DATA_CHECKSUM)
+    return_coll = True
 
     if path:
-        if coll_query:
-            coll_query = coll_query.filter(kw.LIKE(kw.COLL_NAME, path))
+        coll_query = coll_query.filter(kw.LIKE(kw.COLL_NAME, path))
         data_query = data_query.filter(kw.LIKE(kw.COLL_NAME, path))
     for key in key_vals:
         data_query.filter(kw.LIKE(kw.META_DATA_ATTR_NAME, key))
-        if coll_query:
-            coll_query.filter(kw.LIKE(kw.META_COLL_ATTR_NAME, key))
+        coll_query.filter(kw.LIKE(kw.META_COLL_ATTR_NAME, key))
         if key_vals[key]:
             data_query.filter(kw.LIKE(kw.META_DATA_ATTR_VALUE, key_vals[key]))
-            if coll_query:
-                coll_query.filter(kw.LIKE(kw.META_COLL_ATTR_VALUE == key_vals[key]))
+            coll_query.filter(kw.LIKE(kw.META_COLL_ATTR_VALUE, key_vals[key]))
+    if checksum:
+        data_query = data_query.filter(kw.LIKE(kw.DATA_CHECKSUM, checksum))
+        return_coll == False
     # gather results
-    results = [res for res in data_query.get_results()]
-    if coll_query:
-        results.extend([res for res in coll_query.get_results()])
+    results = [tuple(res.values()) for res in data_query.get_results()]
+    if return_coll:
+        results.extend([tuple((list(res.values())[0], '', ''))
+                        for res in coll_query.get_results()])
 
     return results
