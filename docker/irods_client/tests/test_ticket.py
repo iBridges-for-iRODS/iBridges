34196575b742fe030b7ee932197deceab8123585
Raoul Schram <r.d.schram@uu.nl>
2024-02-20 12:21:55 +0100

    Fix yoda tests

--- docker/irods_client/tests/test_ticket.py
+++ docker/irods_client/tests/test_ticket.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 
 import irods
 import pytest
@@ -9,14 +9,15 @@ from ibridges.irodsconnector.tickets import Tickets
 
 @mark.parametrize("item_name", ["collection", "dataobject"])
 @mark.parametrize("ticket_type", ["read", "write"])
-def test_tickets(item_name, ticket_type, session, config, request):
+@mark.parametrize("n_days_ahead", [1, 100])
+def test_tickets(item_name, ticket_type, n_days_ahead, session, config, request):
     item = request.getfixturevalue(item_name)
     ipath = item.path
     tickets = Tickets(session)
     tickets.clear()
     assert len(tickets.update_tickets()) == 0
 
-    exp_date = datetime.today()
+    exp_date = datetime.datetime.today() + datetime.timedelta(days=n_days_ahead)
     tickets.create_ticket(str(ipath), ticket_type=ticket_type, expiry_date=exp_date)
     assert len(tickets.update_tickets()) == 1
     ticket_str = tickets.all_ticket_strings[0]
@@ -26,6 +27,8 @@ def test_tickets(item_name, ticket_type, session, config, request):
     assert ticket_data.name == ticket_str
     assert ticket_data.type == ticket_type
     assert ticket_data.path == str(ipath)
+
+    # It seems that generally irods invalidates the tickets at midnight of the same day
     if config.get("ticket_date_only", False):
         assert ticket_data.expiration_date.date() == exp_date.date()
     else:
