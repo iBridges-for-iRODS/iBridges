f25bd5d782d01b8a64b76fc7649efc0e61523a7b
qubixes <44498096+qubixes@users.noreply.github.com>
2024-02-20 13:24:50 +0100

    Update Tickets class to use datetime as input (#40)
    Also, some refactoring of the class in general.

--- docker/irods_client/tests/test_ticket.py
+++ docker/irods_client/tests/test_ticket.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 
 import irods
 import pytest
@@ -9,26 +9,31 @@ from ibridges.irodsconnector.tickets import Tickets
 
 @mark.parametrize("item_name", ["collection", "dataobject"])
 @mark.parametrize("ticket_type", ["read", "write"])
-def test_tickets(item_name, ticket_type, session, request):
+@mark.parametrize("n_days_ahead", [1, 100])
+def test_tickets(item_name, ticket_type, n_days_ahead, session, config, request):
     item = request.getfixturevalue(item_name)
     ipath = item.path
     tickets = Tickets(session)
     tickets.clear()
-    assert len(tickets.all_tickets(update=True)) == 0
+    assert len(tickets.update_tickets()) == 0
 
-    exp_date = datetime.today().strftime('%Y-%m-%d.%H:%M:%S')
-    tickets.create_ticket(str(ipath), ticket_type=ticket_type, expiry_string=exp_date)
-    assert len(tickets.all_tickets(update=True)) == 1
+    exp_date = datetime.datetime.today() + datetime.timedelta(days=n_days_ahead)
+    tickets.create_ticket(str(ipath), ticket_type=ticket_type, expiry_date=exp_date)
+    assert len(tickets.update_tickets()) == 1
     ticket_str = tickets.all_ticket_strings[0]
     tick = tickets.get_ticket(ticket_str)
     assert isinstance(tick, irods.ticket.Ticket)
-    ticket_data = tickets.all_tickets(update=True)[0]
-    assert ticket_data[0] == ticket_str
-    assert ticket_data[1] == ticket_type
-    assert ticket_data[2] == str(ipath)
-    # assert ticket_data[3] == exp_date
-    assert isinstance(ticket_data[3], str)
+    ticket_data = tickets.update_tickets()[0]
+    assert ticket_data.name == ticket_str
+    assert ticket_data.type == ticket_type
+    assert ticket_data.path == str(ipath)
+
+    # It seems that generally irods invalidates the tickets at midnight of the same day
+    if config.get("ticket_date_only", False):
+        assert ticket_data.expiration_date.date() == exp_date.date()
+    else:
+        assert ticket_data.expiration_date == exp_date
     tickets.delete_ticket(tick)
-    assert len(tickets.all_tickets(update=True)) == 0
+    assert len(tickets.update_tickets()) == 0
     with pytest.raises(KeyError):
         tickets.delete_ticket(tick, check=True)
