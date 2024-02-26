f4b21fd14da3fbdc3c8df390dd419d2049c60839
chstaiger <staigerchristine@gmail.com>
2024-02-26 11:13:11 +0100

    Pylint

--- ibridges/interactive.py
+++ ibridges/interactive.py
@@ -4,7 +4,7 @@
 import json
 import os
 from pathlib import Path
-from typing import Optional, Union
+from typing import Union
 from getpass import getpass
 
 from ibridges.irodsconnector.session import Session
