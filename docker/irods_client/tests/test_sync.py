bf73373abd39f3fa54f202cc01054653c681cd21
maartenschermer <m.d.schermer@uu.nl>
2024-02-26 11:55:42 +0100

    sync test

--- docker/irods_client/tests/test_sync.py
+++ docker/irods_client/tests/test_sync.py
@@ -1,46 +1,12 @@
-import os
-from pathlib import Path
-from ibridges.irodsconnector.sync import sync
+from ibridges.irodsconnector.sync import sync, _calc_checksum
 from ibridges.utils.path import IrodsPath
-from ibridges.irodsconnector.data_operations import create_collection
-
-def _get_local_tree(path, max_level):
-    objects=[]
-    collections=[]
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            fpath=f"{root}/{file}"
-            if max_level is None or fpath.count(os.sep)<max_level:
-                objects.append(fpath[len(str(path)):].lstrip(os.sep))
-        collections.extend([str(Path(root) / dir)[len(str(path)):].lstrip(os.sep)
-            for dir in dirs if max_level is None or dir.count(os.sep)<max_level-1])
-    return objects, collections
-
-def _get_irods_tree(coll, root, level, max_level):
-    root=coll.path if len(root)==0 else root
-    objects=[x.path[len(root):].lstrip('/') for x in coll.data_objects]
-    if max_level is None or level<max_level-1:
-        collections=[x.path[len(root):].lstrip('/') for x in coll.subcollections]
-        for subcoll in coll.subcollections:
-            subobjects, subcollections=_get_irods_tree(
-                coll=subcoll,
-                root=root,
-                level=level+1,
-                max_level=max_level)
-            objects.extend(subobjects)
-            collections.extend(subcollections)
-    else:
-        collections=[]
-    return objects, collections
+from ibridges.irodsconnector.data_operations import create_collection, get_dataobject
 
 def test_sync_upload_download(session, testdata, tmpdir):
     ipath = IrodsPath(session, "~", "empty")
     coll = create_collection(session=session, coll_path=ipath)
 
-    local_obj, local_coll = _get_local_tree(testdata, None)
-    irods_obj, irods_coll = _get_irods_tree(coll, '', 0, None)
-
-    assert (len(irods_obj)+len(irods_coll))==0, "iRODS folder not empty"
+    assert len(coll.data_objects)+len(coll.subcollections)==0, "iRODS folder not empty"
 
     # upload
     sync(session=session,
@@ -52,10 +18,23 @@ def test_sync_upload_download(session, testdata, tmpdir):
          copy_empty_folders=True,
          verify_checksum=True)
 
-    irods_obj, irods_coll = _get_irods_tree(coll, '', 0, None)
+    for cur_file in list(testdata.glob("*")):
+        s_ipath = IrodsPath(session, "~", "empty", cur_file.name)
+        if cur_file.is_file():
+            assert s_ipath.dataobject_exists(), "File not uploaded"
+            cur_obj=get_dataobject(session=session, path=s_ipath)
+            assert _calc_checksum(cur_file)==(cur_obj.checksum
+                if len(cur_obj.checksum)>0
+                else cur_obj.chksum()), "Checksums not identical after upload"
 
-    assert set(local_coll)==set(irods_coll), "Collection upload failed"
-    assert set(local_obj)==set(irods_obj), "Object upload failed"
+    s_ipath = IrodsPath(session, "~/empty/more_data")
+    assert s_ipath.collection_exists(), "Subfolder not uploaded"
+    s_ipath = IrodsPath(session, "~/empty/more_data/polarbear.txt")
+    assert s_ipath.dataobject_exists(), "File in subfolder not uploaded"
+    obj = get_dataobject(session=session, path=s_ipath)
+    assert _calc_checksum(testdata / "more_data" / "polarbear.txt")== \
+        (obj.checksum if len(obj.checksum)>0 else obj.chksum()), \
+            "Checksums not identical after upload"
 
     # download
     sync(session=session,
@@ -67,7 +46,15 @@ def test_sync_upload_download(session, testdata, tmpdir):
         copy_empty_folders=True,
         verify_checksum=True)
 
-    local_obj, local_coll = _get_local_tree(tmpdir, None)
+    for cur_file in list(testdata.glob("*")):
+        if cur_file.is_file():
+            assert (tmpdir / cur_file.name).exists(), "File not downloaded"
+            assert _calc_checksum(tmpdir / cur_file.name)== \
+                _calc_checksum(testdata / cur_file.name), "Checksums not identical after download"
+        elif cur_file.is_dir():
+            assert (tmpdir / cur_file.name).exists(), "Subfolder not downloaded"
 
-    assert set(local_coll)==set(irods_coll), "Collection download failed"
-    assert set(local_obj)==set(irods_obj), "Object download failed"
+    assert (tmpdir / "more_data" / "polarbear.txt").exists(), "File in subfolder not downloaded"
+    assert _calc_checksum(tmpdir / "more_data" / "polarbear.txt")== \
+        _calc_checksum(testdata  / "more_data" / "polarbear.txt"), \
+            "Checksums not identical after download"
