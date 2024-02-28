3fd7c3683f2aa523aae471a5c551fe3dec218198
maartenschermer <m.d.schermer@uu.nl>
2024-02-28 13:32:12 +0100

    sync_data in __init__

--- ibridges/__init__.py
+++ ibridges/__init__.py
@@ -6,6 +6,7 @@ from ibridges.path import IrodsPath
 from ibridges.session import Session
 from ibridges.tickets import Tickets
 from ibridges.search import search_data
+from ibridges.sync import sync_data
 
 __all__ = ["Session", "IrodsPath", "download", "get_collection", "get_dataobject", "upload",
            "MetaData", "Tickets"]
