fbfca7472f62e1a1c1c7d96b52bea85bcc33566c
chStaiger <c.staiger@uu.nl>
2024-05-07 12:18:10 +0200

    Move data objects and collections (#136)
    * Adding moveing data objects and collections
    
    * Adding test for renaming
    
    * Renaming and adjusting tests.

---
+++ docker/irods_client/tests/test_move.py
@@ -0,0 +1,17 @@
+import pytest
+from pytest import mark
+from ibridges.path import IrodsPath
+
+@mark.parametrize("item_name", ["collection", "dataobject"])
+def test_move(session, item_name, request):
+    item = request.getfixturevalue(item_name)
+    old_path = IrodsPath(session, item.path)
+    new_path = IrodsPath(session, '~', 'moved')
+
+    old_path.rename(new_path)
+    assert new_path.exists()
+    assert not old_path.exists()
+
+    new_path.rename(old_path)
+    assert old_path.exists()
+    assert not new_path.exists()
