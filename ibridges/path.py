b32c1ff5f17f67da99bcace835ca4823d27c4401
qubixes <44498096+qubixes@users.noreply.github.com>
2024-05-14 18:56:34 +0200

    Update ibridges/path.py
    Co-authored-by: chStaiger <c.staiger@uu.nl>

--- ibridges/path.py
+++ ibridges/path.py
@@ -278,7 +278,7 @@ class IrodsPath():
         """
         if not self.exists():
             raise ValueError(f"Path '{str(self)}' does not exist;"
-                             "it is neither a collection nor a dataobject")
+                             " it is neither a collection nor a dataobject.")
         if self.dataobject_exists():
             return self.dataobject.size
         all_objs = _get_data_objects(self.session, self.collection)
