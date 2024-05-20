694edf2bda5db8f30714784b1b2645b2757ca414
chstaiger <staigerchristine@gmail.com>
2024-05-20 21:36:11 +0200

    Add missing search for data_name

--- ibridges/search.py
+++ ibridges/search.py
@@ -53,20 +53,32 @@ def search_data(session: Session, path: Optional[Union[str, IrodsPath]] = None,
     # create the query for data objects; we need the collection name, the data name and its checksum
     data_query = session.irods_session.query(icat.COLL_NAME, icat.DATA_NAME,
                                              icat.DATA_CHECKSUM)
+    data_name_query = session.irods_session.query(icat.COLL_NAME, icat.DATA_NAME,
+                                                  icat.DATA_CHECKSUM)
+    # iRODS queries do not know the 'or' operator, so we need three searches
+    # One for the collection, and two for the data 
+    # one data search in case path is a collection path and we want to retrieve all data there
+    # one in case the path is or ends with a file name
     if path:
         coll_query = coll_query.filter(icat.LIKE(icat.COLL_NAME, str(path)))
         data_query = data_query.filter(icat.LIKE(icat.COLL_NAME, str(path)))
+        potential_data_name = path if '/' not in path else path.split("/")[-1]
+        data_name_query = data_name_query.filter(icat.LIKE(icat.DATA_NAME, potential_data_name))
     if key_vals:
         for key in key_vals:
             data_query.filter(icat.LIKE(icat.META_DATA_ATTR_NAME, key))
             coll_query.filter(icat.LIKE(icat.META_COLL_ATTR_NAME, key))
+            data_name_query.filter(icat.LIKE(icat.META_DATA_ATTR_NAME, key))
             if key_vals[key]:
                 data_query.filter(icat.LIKE(icat.META_DATA_ATTR_VALUE, key_vals[key]))
                 coll_query.filter(icat.LIKE(icat.META_COLL_ATTR_VALUE, key_vals[key]))
+                data_name_query = data_name_query.filter(
+                                    icat.LIKE(icat.META_DATA_ATTR_VALUE, key_vals[key]))
     if checksum:
         data_query = data_query.filter(icat.LIKE(icat.DATA_CHECKSUM, checksum))
+        data_name_query = data_name_query.filter(icat.LIKE(icat.DATA_CHECKSUM, checksum))
     # gather results
-    results = list(data_query.get_results())
+    results = list(data_query.get_results())+list(data_name_query.get_results())
     if checksum is None:
         coll_res = list(coll_query.get_results())
         if len(coll_res) > 0:
