a3b9510b37db51911f117d15c6b8b1f822827a9f
chstaiger <staigerchristine@gmail.com>
2024-06-28 14:32:52 +0200

    Prepare v1.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'iBridges'
 copyright = '2024, Christine Staiger'
 author = 'Christine Staiger'
-release = '0.1'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
