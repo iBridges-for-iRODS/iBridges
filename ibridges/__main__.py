fdfceaf35e8c0524b766f1546bc62485fb8ae4a8
chStaiger <c.staiger@uu.nl>
2024-07-10 10:57:35 +0200

    Create irods folder with CLI if it does not exist (#226)

--- ibridges/__main__.py
+++ ibridges/__main__.py
@@ -74,6 +74,9 @@ IBRIDGES_CONFIG_FP = Path.home() / ".ibridges" / "ibridges_cli.json"
 
 def main() -> None:
     """CLI pointing to different entrypoints."""
+    # ensure .irods folder
+    irods_loc = Path.home() / ".irods"
+    irods_loc.mkdir(exist_ok=True)
     # show help by default, else consume first argument
     subcommand = "--help" if len(sys.argv) < 2 else sys.argv.pop(1)
 
