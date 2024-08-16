e23b2282891541c391a962592d8ce27080743913
Staiger, Christine <christine.staiger@wur.nl>
2024-08-16 14:33:07 +0200

    fix tests

--- docker/irods_client/tests/test_ticket.py
+++ docker/irods_client/tests/test_ticket.py
@@ -12,13 +12,13 @@ from ibridges.tickets import Tickets
 @mark.parametrize("n_days_ahead", [1, 100])
 def test_tickets(item_name, ticket_type, n_days_ahead, session, config, request):
     item = request.getfixturevalue(item_name)
-    ipath = item.path
+    ipath = IrodsPath(session, item.path)
     tickets = Tickets(session)
     tickets.clear()
     assert len(tickets.list_tickets()) == 0
 
     exp_date = datetime.datetime.today() + datetime.timedelta(days=n_days_ahead)
-    tickets.create_ticket(str(ipath), ticket_type=ticket_type, expiry_date=exp_date)
+    tickets.create_ticket(ipath, ticket_type=ticket_type, expiry_date=exp_date)
     assert len(tickets.list_tickets()) == 1
     ticket_str = tickets.all_ticket_strings[0]
     tick = tickets.get_ticket(ticket_str)
@@ -26,7 +26,7 @@ def test_tickets(item_name, ticket_type, n_days_ahead, session, config, request)
     ticket_data = tickets.list_tickets()[0]
     assert ticket_data.name == ticket_str
     assert ticket_data.type == ticket_type
-    assert ticket_data.path == str(ipath)
+    assert str(ticket_data.path) == str(ipath)
 
     # It seems that generally irods invalidates the tickets at midnight of the same day
     if config.get("ticket_date_only", False):
