123ed9ccb8f3a3de46411a9ec81bab61a5feb082
chstaiger <staigerchristine@gmail.com>
2024-10-24 05:27:09 +0200

    comments in meta.update tests

--- docker/irods_client/tests/test_meta.py
+++ docker/irods_client/tests/test_meta.py
@@ -74,13 +74,16 @@ def test_meta(item_name, request):
     meta.add('key', 'val', 'test')
     meta.add('key1', 'val1', 'unit1')
 
-    #tests
+    # overwrite value
     meta.update('key', 'val', 'unit', new_value='new_val')
     assert ('key', 'new_val', 'unit') in meta and ('key', 'val', 'unit') not in meta
+    # overwrite units
     meta.update('key', 'new_val', 'unit', new_units='new_unit')
     assert ('key', 'new_val', 'new_unit') in meta and ('key', 'new_val', 'unit') not in meta
+    # overwrite value and units
     meta.update('key', 'new_val', 'new_unit', new_value='val', new_units='unit')
     assert ('key', 'val', 'unit') in meta and ('key', 'new_val', 'new_unit') not in meta
+    # delete units through overwrite with ''
     meta.update('key1', 'val1', 'unit1', new_units='')
     assert ('key1', 'val1') in meta and ('key1', 'val1', 'unit1') not in meta
 
