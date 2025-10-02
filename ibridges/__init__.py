9ccacf3c2bd6dc69ae8e9f5792a7ce9355228796
chStaiger <c.staiger@uu.nl>
2025-10-02 11:54:34 +0200

    add version to init (#363)
    * add version to init
    
    * pylint
    
    * Update ibridges/__init__.py
    
    Co-authored-by: qubixes <44498096+qubixes@users.noreply.github.com>
    
    * remove backport
    
    * pylint
    
    * Update __init__.py
    
    ---------
    
    Co-authored-by: qubixes <44498096+qubixes@users.noreply.github.com>

--- ibridges/__init__.py
+++ ibridges/__init__.py
@@ -1,4 +1,6 @@
-"""iBridges package that implements an API for iRods."""
+"""iBridges package that implements an API for iRODS."""
+
+from importlib.metadata import version
 
 from ibridges.data_operations import download, sync, upload
 from ibridges.meta import MetaData
@@ -6,6 +8,8 @@ from ibridges.path import IrodsPath
 from ibridges.search import search_data
 from ibridges.session import Session
 from ibridges.tickets import Tickets
+
+__version__ = version("ibridges")
 
 __all__ = [
     "Session",
