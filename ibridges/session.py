70ce88a9ee9bfe1fab3061fc89c36c0c28078f4c
chstaiger <staigerchristine@gmail.com>
2026-02-05 07:56:34 +0100

    add expured ccached pw to CAT_INVALID_USER exception.

--- ibridges/session.py
+++ ibridges/session.py
@@ -171,7 +171,6 @@ class Session:  # pylint: disable=too-many-instance-attributes
     def cwd(self, value: str):
         self._cwd = str(value)
 
-
     # Authentication workflow methods
     def has_valid_irods_session(self) -> bool:
         """Check if the iRODS session is valid.
@@ -416,7 +415,9 @@ def _translate_irods_error(exc) -> Exception:  # pylint: disable=too-many-return
     if isinstance(exc, TypeError):
         return LoginError(f"Add info to irods_environment.json: {exc.args}")
     if isinstance(exc, CAT_INVALID_USER):
-        return PasswordError("The provided username and/or password is wrong.")
+        return PasswordError(
+            "Expired cached password or the provided username and/or password is wrong."
+        )
     if isinstance(exc, PAM_AUTH_PASSWORD_FAILED):
         return PasswordError("The provided username and/or password is wrong.")
     if isinstance(exc, CAT_PASSWORD_EXPIRED):
