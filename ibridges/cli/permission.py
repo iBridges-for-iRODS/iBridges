1407990a93c1effd4e8c307be2b1104bf0adcac3
chstaiger <staigerchristine@gmail.com>
2026-02-05 14:30:01 +0100

    add command to manipulate ACLs

--- ibridges/cli/permission.py
+++ ibridges/cli/permission.py
@@ -7,7 +7,7 @@ from ibridges.permissions import Permissions
 class CliACLList(BaseCliCommand):
     """Subcommand to list all permissions of a data object or collection."""
     autocomplete = ["remote_path"]
-    names = ["permission-list"]
+    names = ["permissions"]
     description = "List the permissions of a data object or collection on iRODS."
     examples = ["", "irods:remote_collection"]
 
@@ -40,3 +40,61 @@ class CliACLList(BaseCliCommand):
         print(str(ipath) + ":\n")
         print(perm)
 
+class CliACLEdit(BaseCliCommand):
+    """Subcommand to manipulate the permissions of a data object or collection."""
+
+    autocomplete = ["remote_path"]
+    names = ["set-permission"]
+    description = "Manipulate the permissions of a data object or collection."
+    examples = ["irods:dataobject username read",
+               "irods:dataobject username delete",
+               "irods:collection username read --recursive",]
+
+    @classmethod
+    def _mod_parser(cls, parser):
+        parser.add_argument(
+            "remote_path",
+            help="Path to the data object or collection.",
+            type=str,
+        )
+        parser.add_argument(
+            "user",
+            help="User or group.",
+            type=str,
+        )
+        parser.add_argument(
+            "mode", 
+            help="Access mode. Available: read, write, own, delete.",
+            type=str,
+        )
+        parser.add_argument(
+            "userzone",
+            nargs="?",
+            default="",
+            help="Zone of the user being granted access (optional).",
+        )
+        parser.add_argument(
+            "--recursive",
+            help="If path points to a collection, apply permission changes to all members.",
+            action="store_true"
+        )
+        
+        return parser
+
+
+    @staticmethod
+    def run_shell(session, parser, args):
+        """Manipulate permissions."""
+        ipath = parse_remote(args.remote_path, session)
+        if not ipath.exists():
+            parser.error(f"Path {ipath} does not exist, can't list metadata.")
+            return
+        if ipath.dataobject_exists():
+            perm = Permissions(ipath.session, ipath.dataobject)
+        elif ipath.collection_exists():
+            perm = Permissions(ipath.session, ipath.collection)
+        else:
+            parser.error(f"Path {ipath} is neither a data ibject nor collection.")
+        
+        mode = "null" if args.mode=="delete" else args.mode
+        perm.set(mode, user=args.user,  zone=args.userzone, recursive = args.recursive)
