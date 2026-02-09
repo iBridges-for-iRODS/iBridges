eeae74cb18fc1e85febdc0a268ea4d80e949a4dc
chstaiger <staigerchristine@gmail.com>
2026-02-09 09:39:57 +0100

    remove acl list function

--- ibridges/cli/permission.py
+++ ibridges/cli/permission.py
@@ -5,44 +5,6 @@ from ibridges.cli.util import parse_remote
 from ibridges.permissions import Permissions
 
 
-class CliACLList(BaseCliCommand):
-    """Subcommand to list all permissions of a data object or collection."""
-
-    autocomplete = ["remote_path"]
-    names = ["perm"]
-    description = "List the permissions of a data object or collection on iRODS."
-    examples = ["", "irods:remote_collection"]
-
-    @classmethod
-    def _mod_parser(cls, parser):
-        parser.add_argument(
-            "remote_path",
-            help="iRODS path for listing permissions.",
-            type=str,
-            default=".",
-            nargs="?",
-        )
-        return parser
-
-    @staticmethod
-    def run_shell(session, parser, args):
-        """List the permissions of a data object or collection."""
-        ipath = parse_remote(args.remote_path, session)
-        if not ipath.exists():
-            parser.error(f"Path {ipath} does not exist, can't list permissions.")
-            return
-        if ipath.dataobject_exists():
-            perm = Permissions(ipath.session, ipath.dataobject)
-        elif ipath.collection_exists():
-            perm = Permissions(ipath.session, ipath.collection)
-        else:
-            parser.error(f"Path {ipath} is neither a data object nor collection.")
-            return
-
-        print(str(ipath) + ":\n")
-        print(perm)
-
-
 class CliACLEdit(BaseCliCommand):
     """Subcommand to manipulate the permissions of a data object or collection."""
 
