5b8a00db442a68a9acb7f9501e312a9d7e0bf130
chstaiger <staigerchristine@gmail.com>
2026-02-09 09:38:22 +0100

    Refactor ACL listing and improve recursive/object handling
    - Add ACLs to list command
    - Refactor listing logic for objects and recursive mode
    - Remove obsolete ACL list function
    - Adjust internal ordering for consistent output

--- ibridges/cli/permission.py
+++ ibridges/cli/permission.py
@@ -5,44 +5,6 @@ from ibridges.cli.util import parse_remote
 from ibridges.permissions import Permissions
 
 
-class CliACLList(BaseCliCommand):
-    """Subcommand to list all permissions of a data object or collection."""
-
-    autocomplete = ["remote_path"]
-    names = ["perm"]
-    description = "List the permissions of a data object or collection on iRODS."
-    examples = ["", "irods:remote_collection"]
-
-    @classmethod
-    def _mod_parser(cls, parser):
-        parser.add_argument(
-            "remote_path",
-            help="iRODS path for listing permissions.",
-            type=str,
-            default=".",
-            nargs="?",
-        )
-        return parser
-
-    @staticmethod
-    def run_shell(session, parser, args):
-        """List the permissions of a data object or collection."""
-        ipath = parse_remote(args.remote_path, session)
-        if not ipath.exists():
-            parser.error(f"Path {ipath} does not exist, can't list permissions.")
-            return
-        if ipath.dataobject_exists():
-            perm = Permissions(ipath.session, ipath.dataobject)
-        elif ipath.collection_exists():
-            perm = Permissions(ipath.session, ipath.collection)
-        else:
-            parser.error(f"Path {ipath} is neither a data object nor collection.")
-            return
-
-        print(str(ipath) + ":\n")
-        print(perm)
-
-
 class CliACLEdit(BaseCliCommand):
     """Subcommand to manipulate the permissions of a data object or collection."""
 
@@ -50,37 +12,35 @@ class CliACLEdit(BaseCliCommand):
     names = ["chmod"]
     description = "Manipulate the permissions of a data object or collection."
     examples = [
-        "irods:dataobject read username",
-        "irods:dataobject delete username",
-        "irods:collection read username --recursive",
-        "irods:collection inherit",
+        "read username dataobject",
+        "delete username dataobject",
+        "read username collection --recursive",
+        "inherit collection",
+        "read username userzone dataobject",
     ]
 
     @classmethod
     def _mod_parser(cls, parser):
         parser.add_argument(
+            "mode",
+            help="Access mode. Available: read, write, own, delete, inherit, noinherit",
+            type=str,
+            choices=["read", "write", "own", "delete", "inherit", "noinherit"],
+        )
+        parser.add_argument(
+            "user_info",
+            help="User or group optionally followed by the zone name.",
+            nargs="*",
+            type=str,
+            default=""
+        )
+
+        parser.add_argument(
             "remote_path",
             help="Path to the data object or collection.",
             type=str,
         )
-        parser.add_argument(
-            "mode",
-            help="Access mode. Available: read, write, own, delete, inherit, noinherit",
-            type=str,
-            choices=["read", "write", "own", "delete", "inherit"],
-        )
-        parser.add_argument(
-            "user",
-            help="User or group.",
-            nargs="?",
-            type=str,
-        )
-        parser.add_argument(
-            "userzone",
-            nargs="?",
-            default="",
-            help="Zone of the user being granted access (optional).",
-        )
+
         parser.add_argument(
             "-l", help="List permissions after applying changes.", action="store_true"
         )
@@ -105,6 +65,17 @@ class CliACLEdit(BaseCliCommand):
             parser.error(f"Path {ipath} does not exist, can't list permissions.")
             return
 
+        user = None
+        userzone = ""
+
+        if len(args.user_info) == 1:
+            user = args.user_info[0]
+        elif len(args.user_info) == 2:
+            user, userzone = args.user_info
+        elif len(args.user_info) > 2:
+            parser.error("Too many arguments before remote_path")
+
+
         if ipath.dataobject_exists():
             perm = Permissions(ipath.session, ipath.dataobject)
         elif ipath.collection_exists():
@@ -113,7 +84,7 @@ class CliACLEdit(BaseCliCommand):
             parser.error(f"Path {ipath} is neither a data object nor collection.")
             return
 
-        if args.mode != "inherit" and not args.user:
+        if  "inherit" not in args.mode and not user:
             parser.error("The following arguments are required: user [userzone]")
             return
         if "inherit" in args.mode and not ipath.collection_exists():
@@ -121,7 +92,7 @@ class CliACLEdit(BaseCliCommand):
             return
 
         mode = "null" if args.mode == "delete" else args.mode
-        perm.set(mode, user=args.user, zone=args.userzone, recursive=args.recursive)
+        perm.set(mode, user=user, zone=userzone, recursive=args.recursive)
 
         if args.l:
             print(perm)
--- ibridges/cli/shell.py
+++ ibridges/cli/shell.py
@@ -15,7 +15,7 @@ except ImportError:
 from ibridges.cli.data_operations import CliDownload, CliMakeCollection, CliRm, CliSync, CliUpload
 from ibridges.cli.meta import CliMetaAdd, CliMetaDel, CliMetaList
 from ibridges.cli.navigation import CliCd, CliGui, CliList, CliPwd, CliSearch, CliTree, CliVersion
-from ibridges.cli.permission import CliACLEdit, CliACLList
+from ibridges.cli.permission import CliACLEdit
 from ibridges.cli.util import cli_authenticate
 from ibridges.path import IrodsPath
 
@@ -35,7 +35,6 @@ ALL_BUILTIN_COMMANDS = [
     CliSync,
     CliGui,
     CliVersion,
-    CliACLList,
     CliACLEdit,
 ]
 IBSHELL_HISTORY_FILE = Path.home() / ".ibridges" / ".shell_history"
