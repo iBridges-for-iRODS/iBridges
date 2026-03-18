5ae35b4e7b354171ef3083a47c495f502f6f1585
Raoul Schram <r.d.schram@uu.nl>
2026-03-18 12:33:16 +0100

    Fix test

--- docker/irods_client/tests/test_data_ops.py
+++ docker/irods_client/tests/test_data_ops.py
@@ -240,7 +240,7 @@ def test_meta_down_upload(session, testdata, tmpdir):
     assert len(ops.meta_download) == 1
     assert len(ops.meta_download[0][2]) == 1
     print(output)
-    assert f"{meta_fp} -> {ipath}" in output
+    assert f"{ipath} -> {meta_fp}" in output
 
     ops = upload(tmpdir/"test", ipath, overwrite=True, metadata=meta_fp, dry_run=True)
     print("DEBUG", ops.meta_upload)
