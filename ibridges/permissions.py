b08971f5eea4790735d717a9f28ef4c3d1a42240
chStaiger <c.staiger@uu.nl>
2026-08-12 18:57:57 +0200

    Bugfix: add inheritance flag to print function for acls (#420)
    * add inheritance flag to print acls
    
    * ruff
    
    * Update ibridges/permissions.py
    
    Co-authored-by: qubixes <44498096+qubixes@users.noreply.github.com>
    
    * Update ibridges/permissions.py
    
    Co-authored-by: qubixes <44498096+qubixes@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: qubixes <44498096+qubixes@users.noreply.github.com>

--- ibridges/permissions.py
+++ ibridges/permissions.py
@@ -7,6 +7,8 @@ import irods.collection
 import irods.exception
 import irods.session
 
+from ibridges.path import IrodsPath
+
 
 class Permissions:
     """Irods permissions operations.
@@ -46,6 +48,14 @@ class Permissions:
             names.append(_format_name(perm))
             zones.append(perm.user_zone)
             permissions.append(perm.access_name)
+
+        # add inheritance
+        if hasattr(self.item, "inheritance"):
+            # Fetch collection again to update attribute
+            path = IrodsPath(self.session, self.item.path)
+            names.append("")
+            zones.append("")
+            permissions.append("inheritance: " + str(path.collection.inheritance))
 
         return _create_table(names, zones, permissions)
 
